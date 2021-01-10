63e473969b7b46ca72a20f9e6f43b564fc99ed8a
a.solntsev <a.solntsev@corp.mail.ru>
2021-01-10 12:20:32 +0300

    Database settings for postgress added, initial migrations applied.

--- arkenstone/arkenstone/settings.py
+++ arkenstone/arkenstone/settings.py
@@ -76,10 +76,21 @@ WSGI_APPLICATION = 'arkenstone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ark_test_001',
+        'USER': 'postgres',
+        'PASSWORD': 'lake90distr',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
