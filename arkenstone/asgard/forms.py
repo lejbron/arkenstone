33b1f69998a5e3b1ae6ae8dabfb39a158c86258f
a.solntsev <a.solntsev@corp.mail.ru>
2021-01-24 13:47:35 +0300

    Signup form changed - two model forms used.

--- arkenstone/asgard/forms.py
+++ arkenstone/asgard/forms.py
@@ -1,18 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from players.models import Profile
 
 
-class SignUpForm(UserCreationForm):
-    nickname = forms.CharField(
-        label='nickname',
-        help_text='Your tournament name')
-    army = forms.CharField(
-        label='army',
-        help_text='Enter if you have one')
-    birth_date = forms.DateField(
-        help_text='Required. Format: YYYY-MM-DD')
-
+class UserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ('username', 'nickname', 'army', 'birth_date', 'password1', 'password2', )
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('nickname', 'army', 'birth_date')
--- arkenstone/asgard/views.py
+++ arkenstone/asgard/views.py
@@ -1,20 +1,53 @@
-from asgard.forms import SignUpForm
+from django.contrib import messages
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 from django.shortcuts import redirect, render
+
+from .forms import ProfileForm, UserForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = SignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            user.refresh_from_db()  # load the profile instance created by the signal
-            user.profile.birth_date = form.cleaned_data.get('birth_date')
-            user.save()
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=user.username, password=raw_password)
-            login(request, user)
-            return redirect('index')
+        user_form = UserForm(request.POST)
+        if user_form.is_valid():
+            user = user_form.save()
+            profile_form = ProfileForm(request.POST, instance=user.profile)
+            if profile_form.is_valid():
+                profile_form.save()
+                user.refresh_from_db()  # load the profile instance created by the signal
+                raw_password = user_form.cleaned_data.get('password1')
+                user = authenticate(username=user.username, password=raw_password)
+                login(request, user)
+                return redirect('index')
+        else:
+            profile_form = ProfileForm(request.POST)
     else:
-        form = SignUpForm()
-    return render(request, 'signup.html', {'form': form})
+        user_form = UserForm()
+        profile_form = ProfileForm()
+    return render(request, 'signup.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
+
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == 'POST':
+        user_form = UserForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, 'Your profile was successfully updated!')
+            return redirect('index')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        user_form = UserForm(instance=request.user)
+        profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'profiles/profile.html', {
+        'user_form': user_form,
+        'profile_form': profile_form
+    })
