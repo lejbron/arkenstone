050df60f0faabb5a0d93157f5f526f86c83d7785
a.solntsev <a.solntsev@corp.mail.ru>
2021-03-03 22:20:39 +0300

    Heroku first deploy.

--- arkenstone/arkenstone/settings.py
+++ arkenstone/arkenstone/settings.py
@@ -4,21 +4,29 @@ Django settings for arkenstone project.
 
 import os
 
+import dj_database_url
 from decouple import Csv, config
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', cast=bool)
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT'),
+DEBUG = config('DEBUG', default=False, cast=bool)
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
+        }
     }
-}
+if not DEBUG:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=config('DATABASE_URL')
+        )
+    }
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
