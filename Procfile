6a03b80cd5e225d8c9e833cc2844ddc1ac3102de
a.solntsev <a.solntsev@corp.mail.ru>
2021-03-04 10:23:55 +0300

    Heroku first deplou try 1.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn arkenstone/arkenstone.wsgi --log-file -
--- arkenstone/arkenstone/settings.py
+++ arkenstone/arkenstone/settings.py
@@ -95,28 +95,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'arkenstone.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ark_090_test_1',
-        'USER': 'postgres',
-        'PASSWORD': 'lake90distr',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
@@ -155,8 +133,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
 LOGIN_URL = '/auth/login/'
 LOGIN_REDIRECT_URL = 'index'
 # LOGOUT_REDIRECT_URL = "index"
@@ -165,12 +141,12 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#    os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
