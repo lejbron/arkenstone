8faad761864a7059653465db76266d9f8c44a44b
a.solntsev <a.solntsev@corp.mail.ru>
2021-03-04 15:19:43 +0300

    Static files settings try 7.

--- arkenstone/settings/base.py
+++ arkenstone/settings/base.py
@@ -107,4 +107,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'live-static-files', 'static-root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'live-static', 'static-root')
