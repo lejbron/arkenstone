2e60886ae956b0b4e05c6c3b3df47b3d7ddb1444
a.solntsev <a.solntsev@corp.mail.ru>
2021-03-17 20:45:13 +0300

    best_practices updated

--- tournaments/views/__init__.py
+++ tournaments/views/__init__.py
@@ -1,7 +1,9 @@
 from .data_input_forms import input_tour_results  # noqa
 from .data_input_forms import register_on_tournament  # noqa
-from .function_based_views import tournament_detail_view  # noqa; noqa
+from .function_based_views import match_detail_view  # noqa
+from .function_based_views import tour_detail_view  # noqa
+from .function_based_views import tournament_detail_view  # noqa
 from .function_based_views import tournaments_list_view  # noqa
-from .function_based_views import match_detail_view, tour_detail_view
-from .status_change_forms import start_tournament  # noqa; noqa
-from .status_change_forms import finish_tour, start_tour
+from .status_change_forms import finish_tour  # noqa
+from .status_change_forms import start_tour  # noqa
+from .status_change_forms import start_tournament  # noqa
