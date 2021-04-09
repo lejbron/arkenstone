264a064366fef04c818f0b6ecf392817e3a4a1d3
a.solntsev <a.solntsev@corp.mail.ru>
2021-04-09 13:10:58 +0300

    Replace registered_players with related_name

--- tournaments/models.py
+++ tournaments/models.py
@@ -89,13 +89,6 @@ class Tournament(models.Model):
         verbose_name='Slug'
     )
 
-    @property
-    def registered_players(self):
-        try:
-            return PlayerStats.objects.filter(tournament=self)
-        except PlayerStats.DoesNotExist:
-            print('No players registered yet')
-
     class Meta:
         ordering = [('-start_date'), ]
 
@@ -247,7 +240,7 @@ class Tour(models.Model):
         Attributes:
             players: Список зарегистриованных на турнир игроков.
         """
-        players = list(self.tournament.registered_players)
+        players = list(self.tournament.registered_players.all())
         for i in range(0, len(players), 2):
             m = Match(
                 tour=self,
